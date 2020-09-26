40760e578eadfa50f0ee3e3ff195609c4da794a8
todes-l33ter <70980402+todes-l33ter@users.noreply.github.com>
2020-09-26 23:01:28 +0200

    some new aliases

--- bash_aliases_real.sh
+++ bash_aliases_real.sh
@@ -26,5 +26,44 @@ alias du='du -ch'
 
 # top is atop, just like vi is vim
 alias top='atop'
+
+# file tree
+alias tr="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+
+# list folders by size in current directory
+alias gross="du -h --max-depth=1 | sort -rh"
+
+# # count # of lines
+alias wcl='wc -l'
+
+# Find a file from the current directory
+alias ff='find . -name '
+
+alias rmf='rm -rf -i'
+
+#this will alias two commands mkdir and cd to one mkcd command
+alias mkcd='mkdircd(){ mkdir $1; cd $1; }; mkdircd'
+
+# starts nano with line number enabled
+alias nano='nano -c'
+
+# Sort directories by sizes
+alias dush='du -h --max-depth=1 | sort -h'
+
+# Can't see all the files in one page ?
+alias lsless='ls | less'
+
+# Finding the 20 lagrest directories
+alias dudu='du -S / | sort -nr | head -n50'
+
+# CD und LS
+function cdls()
+{
+  builtin cd $*
+  ls -lhaG
+}
+
+alias cdls='cdls'
+
 E0F
 bash
